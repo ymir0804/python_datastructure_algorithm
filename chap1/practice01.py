86aa7ce2dfa9fa43095c4577577c0d438286425d
Your Name <you@example.com>
2020-08-21 16:59:40 +0900

    practice_01

--- chap1/practice01.py
+++ chap1/practice01.py
@@ -9,9 +9,25 @@ def convert_to_decimal(num, base):
 
 def test_convert_to_decimal():
     num, base = 1001, 2
-    assert(convert_to_decimal(num, base) == 9)
+    assert (convert_to_decimal(num, base) == 9)
+    print('테스트 통과')
+
+
+def convert_from_decimal(decimal, base):
+    multiplier, result = 1, 0
+    while decimal > 0:
+        result += decimal % base * multiplier
+        multiplier *= 10
+        decimal = decimal // base
+    return result
+
+
+def test_convert_from_decimal():
+    num, base = 9, 2
+    assert (convert_from_decimal(num, base) == 1001)
     print('테스트 통과')
 
 
 if __name__ == "__main__":
     test_convert_to_decimal()
+    test_convert_from_decimal()
