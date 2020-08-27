6430a5337cc3e395ac1ad808e6eae38ab4123b16
duswn916 <duswn916@kakao.com>
2020-08-27 14:56:58 +0900

    remove_blank_lines_with

---
+++ chap4/remove_blank_lines_with.py
@@ -0,0 +1,31 @@
+import sys
+
+
+def read_data(filename):
+    lines = []
+    with open(filename) as fh:
+        for line in fh:
+            if line.strip():
+                lines.append(line)
+    return lines
+
+
+def write_data(lines, filename):
+    fh = None
+    with open(filename, "w") as fh:
+        for line in lines:
+            fh.write(line)
+
+
+def remove_blank_lines():
+    if len(sys.argv) < 2:
+        print(f"usage: python {sys.argv[0]}")
+
+    for filename in sys.argv[1:]:
+        lines = read_data(filename)
+        if lines:
+            write_data(lines, filename)
+
+
+if __name__ == '__main__':
+    remove_blank_lines()
